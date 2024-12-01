59da6c134181304604adb46cbca4c20fa57c6165
florrgonzalez <florgonzalez0210@gmail.com>
2024-12-01 19:59:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ for (let i = 0; i < totalLogos; i++) {
 const animationDuration = 15 + totalLogos * 0.5; // Ajusta la duración según la cantidad de logos
 logoContainer.style.animationDuration = `${animationDuration}s`; // Establecer la duración de la animación
 
+
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+// when we click on hamburger icon its close 
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
